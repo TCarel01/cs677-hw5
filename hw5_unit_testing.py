0680104eaca7d503fb5ef7259f29dc33319ac4ba
dpoulin00 <dpoulin1028@gmail.com>
2026-05-02 17:42:29 -0400

    Finished trader unit tests

--- hw5_unit_testing.py
+++ hw5_unit_testing.py
@@ -47,7 +47,7 @@ class TestWarehouse(unittest.TestCase):
         # Set up test socket for warehouse node to send msgs to
         self.socket = socket.socket()
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.socket.settimeout(100)  # Time out so we can gracefully exit once we stop seeing messages.
+        self.socket.settimeout(10)  # Time out so we can gracefully exit once we stop seeing messages.
         self.socket.bind((socket.gethostname(), self.test_port))
         self.socket.listen(100000)
         # set up thread testing
@@ -194,7 +194,7 @@ class TestWarehouse(unittest.TestCase):
         return
 
 
-class TestTrader(unittest.TestCase):
+class TestNonCachingTrader(unittest.TestCase):
     synchronized = True
     def setUp(self):
         """
@@ -218,13 +218,13 @@ class TestTrader(unittest.TestCase):
         # Set up test node socket for trader node to send msgs to
         self.node_socket = socket.socket()
         self.node_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.node_socket.settimeout(100)  # Time out so we can gracefully exit once we stop seeing messages.
+        self.node_socket.settimeout(10)  # Time out so we can gracefully exit once we stop seeing messages.
         self.node_socket.bind((socket.gethostname(), self.node_port))
         self.node_socket.listen(100000)
         # Set up test warehouse socket for trader node to send msgs to
         self.wh_socket = socket.socket()
         self.wh_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.wh_socket.settimeout(100)  # Time out so we can gracefully exit once we stop seeing messages.
+        self.wh_socket.settimeout(10)  # Time out so we can gracefully exit once we stop seeing messages.
         self.wh_socket.bind((socket.gethostname(), self.warehouse_port))
         self.wh_socket.listen(100000)
         # set up testing either using threads or processes
@@ -277,7 +277,20 @@ class TestTrader(unittest.TestCase):
         send_msg(outgoing_msg, dest_port=self.trader_id)
         return
     
+    def warehouse_send_cache(self, fish:int, salt:int, boar:int):
+        totals = {
+            "FISH": fish,
+            "SALT": salt,
+            "BOAR": boar,
+        }
+        msg = dict(
+            type = enums.MsgType.SYNC_DATA.name,
+            totals = totals
+        )
+        send_msg(msg, dest_port=self.trader_id)
+    
     def test_buy_is_fwded(self):
+        # Test that a BUY request is forwarded
         uid = uuid.uuid4()
         self.node_send_msg(uid=uid, type=enums.MsgType.BUY.name, item=enums.Item.SALT.name, quantity=5)
         reply_msg = recieve_msg(self.wh_socket)
@@ -290,6 +303,70 @@ class TestTrader(unittest.TestCase):
                               passed_cache=True)
         self.assertDictEqual(reply_msg, expected_reply)
         return
+    
+    def test_restock_is_fwded(self):
+        # Test that a RESTOCK request is forwarded
+        uid = uuid.uuid4()
+        self.node_send_msg(uid=uid, type=enums.MsgType.RESTOCK.name, item=enums.Item.SALT.name, quantity=5)
+        reply_msg = recieve_msg(self.wh_socket)
+        expected_reply = dict(uid=uid,
+                              sender=self.trader_id,
+                              type=enums.MsgType.RESTOCK.name,
+                              item=enums.Item.SALT.name,
+                              quantity=5,
+                              peer_id=self.node_id,
+                              passed_cache=True)
+        self.assertDictEqual(reply_msg, expected_reply)
+        return
+    
+    def test_reply_restock_is_forwarded(self):
+        print("Skipping reply fwd test, as currently, the script is set up such that replies to RESTOCKs are not fwded to sellers")
+        return
+        uid = uuid.uuid4()
+        self.warehouse_send_msg(uid=uid, type=enums.MsgType.RESTOCK_REPLY.name, item=enums.Item.SALT.name, quantity=5)
+        reply_msg = recieve_msg(self.node_socket)
+        expected_reply = dict(uid=uid,
+                              sender=self.trader_id,
+                              type=enums.MsgType.RESTOCK_REPLY.name,
+                              item=enums.Item.SALT.name,
+                              quantity=5,
+                              peer_id=self.node_id,
+                              passed_cache=True)
+        self.assertDictEqual(reply_msg, expected_reply)
+
+    def test_reply_buy_is_forwarded(self):
+        uid = uuid.uuid4()
+        self.warehouse_send_msg(uid=uid, type=enums.MsgType.BUY_REPLY.name, item=enums.Item.SALT.name, quantity=5)
+        reply_msg = recieve_msg(self.node_socket)
+        expected_reply = dict(uid=uid,
+                              sender=self.trader_id,
+                              type=enums.MsgType.BUY_REPLY.name,
+                              item=enums.Item.SALT.name,
+                              quantity=5,
+                              peer_id=self.node_id,
+                              passed_cache=True)
+        self.assertDictEqual(reply_msg, expected_reply)
+    
+class TestCachingTrader(TestNonCachingTrader):
+    synchronized=False
+
+    def test_buy_not_fwded(self):
+        # Test that a BUY request is not forwarded when the cache has that item at 0
+        uid = uuid.uuid4()
+        self.warehouse_send_cache(fish=0, salt=0, boar=0)
+        self.node_send_msg(uid=uid, type=enums.MsgType.BUY.name, item=enums.Item.SALT.name, quantity=5)
+        reply_msg = recieve_msg(self.node_socket)
+        expected_reply = dict(uid=uid,
+                              sender=self.trader_id,
+                              type=enums.MsgType.BUY.name,
+                              item=enums.Item.SALT.name,
+                              quantity=0,
+                              peer_id=self.node_id,
+                              passed_cache=False)
+        self.assertDictEqual(reply_msg, expected_reply)
+        return
+
+        
         
 
 
@@ -297,7 +374,9 @@ class TestTrader(unittest.TestCase):
 if __name__ == "__main__":
     #unittest.main()
     warehouse_suite = unittest.TestLoader().loadTestsFromTestCase(TestWarehouse)
-    trader_suite = unittest.TestLoader().loadTestsFromTestCase(TestTrader)
+    caching_trader_suite = unittest.TestLoader().loadTestsFromTestCase(TestNonCachingTrader)
+    non_caching_trader_suite = unittest.TestLoader().loadTestsFromTestCase(TestNonCachingTrader)
     #unittest.TextTestRunner().run(warehouse_suite)
-    unittest.TextTestRunner().run(trader_suite)
+    #unittest.TextTestRunner().run(non_caching_trader_suite)
+    unittest.TextTestRunner().run(caching_trader_suite)
 
